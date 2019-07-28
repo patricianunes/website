61b361a64c4a5021cd7e467d8e5d3ccf493d454f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-28 00:46:12 +0000

    Deploy patricianunes/website to github.com/patricianunes/website.git:gh-pages

---
+++ precache-manifest.933f6474c2970edde87797faf631cbea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2894b6be35b4131abeacf791e748eba5",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "3c9e6ab474c3885cb9e5",
+    "url": "/website/static/css/main.8187f99b.chunk.css"
+  },
+  {
+    "revision": "312c5fcbd0d2df2d7c83",
+    "url": "/website/static/js/2.f814d1d6.chunk.js"
+  },
+  {
+    "revision": "3c9e6ab474c3885cb9e5",
+    "url": "/website/static/js/main.29823693.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "40d06eb43ee2dc44749aa5fd9c21752f",
+    "url": "/website/static/media/bjjking.40d06eb4.png"
+  },
+  {
+    "revision": "493b391d61dd28dcccb0df62a703f1f9",
+    "url": "/website/static/media/ce.493b391d.png"
+  },
+  {
+    "revision": "c381a8a8806b83f696fb1f3977b15c8d",
+    "url": "/website/static/media/tictactoe.c381a8a8.png"
+  }
+]);
